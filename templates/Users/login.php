6fcca62247d0f9a90b65769a04eefb13dce3b90d
mvlinowskv <klaudiaa.malinowskaa@gmail.com>
2021-09-13 13:25:20 +0200

    change scss

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,19 +1,25 @@
 <!-- in /templates/Users/login.php -->
-<div class="users form">
-<?
+<section class="background-center background-cover background-fixed background-login w-100 h-100vh d-flex align-items-center">
+    <div class="box-content w-90 w-lg-50 mx-auto">
+        <h1 class="text-center text-uppercase color-white font-h1 mb-4">L<span class="border-bottom">ogi</span>n</h1>
+        <div class="users form bg-black p-4 color-white text-uppercase">
+            <?= $this->Flash->render() ?>
+            
+            <?= $this->Form->create() ?>
+            <fieldset class="w-lg-75 w-100 mx-auto">
+                <?= $this->Form->control('email', ['required' => true, 'class' => 'w-75 w-lg-75 float-right']) ?>
+                <?= $this->Form->control('password', ['required' => true, 'class' => 'w-75 w-lg-75 float-right']) ?>
 
+                <?= $this->Form->submit(__('Login'), ['class' => 'float-right w-50 bg-red py-2 mt-3 border-0 text-uppercase']); ?>
+                <?= $this->Form->end() ?>
 
-?>
-    <?= $this->Flash->render() ?>
-    <h3>Login</h3>
-    <?= $this->Form->create() ?>
-    <fieldset>
-        <legend><?= __('Please enter your username and password') ?></legend>
-        <?= $this->Form->control('email', ['required' => true]) ?>
-        <?= $this->Form->control('password', ['required' => true]) ?>
-    </fieldset>
-    <?= $this->Form->submit(__('Login')); ?>
-    <?= $this->Form->end() ?>
+                <div class="mt-3 color-white">
+                    
+                </div>
+                
+            </fieldset>
+            
+        </div>
+    </div>
 
-    <?= $this->Html->link("Add User", ['action' => 'add']) ?>
-</div>
+</section>
